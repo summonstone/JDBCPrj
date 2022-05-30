2b12d180f066c88f04e1127e7d3d1d70ddc6586e
Seows <nasla416@naver.com>
2022-05-30 15:04:40 +0900

    try

--- src/ex1/Program1.java
+++ src/ex1/Program1.java
@@ -1,8 +1,24 @@
 package ex1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Program1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		String url = "jdbc:oracle:thin:localhose/xepdb1";
+
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con = DriverManager.getConnection(url, "newlec", "admin");
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(url);
+
+		rs.close();
+		st.close();
+		con.close();
 
 	}
 }
